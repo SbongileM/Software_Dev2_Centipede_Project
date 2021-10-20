3033b10cf3f11eefd3ff7dbb68140b9aaa159a00
Lethiwe Mchunu <mchunulethiwe6@gmail.com>
2021-10-21 01:43:59 +0200

    Drawtext updated

--- src/game-source-code/DrawText.h
+++ src/game-source-code/DrawText.h
@@ -13,28 +13,12 @@ using namespace std;
 class DrawText
 {
 public:
-    /** \brief Default constructor
-     *
-     * Initialises the text font and colour.
-     */
     DrawText();
-    /** \brief Draws the text given to the window at the specified position and size.
-     *
-     * \param text string
-     * \param textSize int
-     * \param position Vector2f
-     * \param window RenderWindow&
-     */
+
     void displayText(string, int, Vector2f, RenderWindow &);
-    /** \brief Changes the color of the text
-     *
-     * \param color Color
-     */
-    void setTextColor(Color);
-    /** \brief Changes the font of the text
-     *
-     * \param newFont string
-     */
+
+    void setTextColor(Color newColor){color = newColor;};
+
     void setFont(string);
 
 private:
