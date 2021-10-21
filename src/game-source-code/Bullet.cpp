e088bbe678966e98335bb48962a7260673eabe59
Sbongile Mdaki <Sbongilep4@gmail.com>
2021-10-21 06:47:36 +0200

    Updated Bullet object class

--- src/game-source-code/Bullet.cpp
+++ src/game-source-code/Bullet.cpp
@@ -5,40 +5,25 @@ using std::end;
 #include "Bullet.h"
 #include "Configuration.h"
 
-Bullet::Bullet(float x, float y, Direction dir)
+Bullet::Bullet(int x,int y)
 {
     _position = Position();
     _position.push_back(x);
     _position.push_back(y);
-
-    _direction = dir;
     _lifePoints = 20;
     _speed = DEFAULTSPEED;
     _state = ALIVE;
 }
 
-void Bullet::move()
+void Bullet::moveBullet()
 {
-    if (_direction == UP)
-    {
-        moveUp();
-    }
-    else
-    {
-        throw std::invalid_argument("TRYING TO MOVE IN INVALID DIRECTION");
-    }
+    moveUp();
 }
-
 
 void Bullet::moveUp()
 {
-    if ( _position[1] > 0 )
-    {
-        _position[1] -= _speed;
-    }
-    else
-    {
-        _state = DEAD;
-    }
+    if ( _position[1] > 0 ){_position[1] -= _speed;}
+
+    else{_state = DEAD;}
 }
 
