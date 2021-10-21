3d273ccf9173d1f9fe0d482fd61b914e348674db
Sbongile Mdaki <Sbongilep4@gmail.com>
2021-10-21 07:26:11 +0200

    Creating mushroom interface

--- src/game-source-code/Mushroom.h
+++ src/game-source-code/Mushroom.h
@@ -1,16 +1,37 @@
 #ifndef MUSHROOM_H
 #define MUSHROOM_H
+#include <vector>
+#include <memory>
+#include "Utils.h"
 
+using namespace std;
 
 class Mushroom
 {
     public:
-        Mushroom();
-        ~Mushroom();
+        Mushroom(int x, int y);
 
-    protected:
+        bool isAlive();
+
+        bool isDead();
+
+        int getXpos(){return xPosition;};
+
+        int getYpos(){return yPosition;};
+
+        int getLives(){return lives;};
+
+        void setState(State state);
+
+        void setLives(int _lives);
 
     private:
+        int xPosition;
+        int yPosition;
+        State state_of_mushroom;
+        int lives;
+
 };
 
+typedef vector<shared_ptr<Mushroom>> Mushrooms;
 #endif // MUSHROOM_H
