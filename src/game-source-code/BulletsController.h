912b0b74d3d6c0d8d38b531d346976dede24d7e3
Sbongile Mdaki <Sbongilep4@gmail.com>
2021-10-21 06:50:02 +0200

    Updated Bullet Contoller

--- src/game-source-code/BulletsController.h
+++ src/game-source-code/BulletsController.h
@@ -1,45 +1,26 @@
 #ifndef BULLE0ULLETCONTROLLER_H
 #include <vector>
 #include "Bullet.h"
+#include "Utils.h"
 using std::vector;
 typedef vector<BulletPtr> BulletList;
 typedef vector<BulletPtr>::iterator BulletsListIter;
 
 class BulletsController
 {
- public:
-  /** \brief A function which adds a new Bullet to the list of
-   *  bullets as it is created. It takes in the x and y position
-   *  co-ordinate of the bullet as well as the direction.
-   *
-   * \param float
-   * \param float
-   * \param Direction
-   */
-  void add(float, float, Direction);
+public:
+    void add(float, float);
 
-  /** \brief  It returns the list of bullets.
-   *
-   * \return BulletList
-   *
-   */
-  BulletList getBullets() {return _bullets; };
+    BulletList getBullets(){return _bullets;};
 
+    void update();
 
-  /** \brief Moves the bullets and removes 'dead' bullets
-   *
-   */
-  void update();
+    void setBullets(BulletList);
 
+    void deleteDeadBullets(BulletList&);
 
-  /** \brief Creates a list of bullets
-   *
-   * \param BulletList
-   */
-  void setBullets(BulletList );
-
- private:
-  BulletList _bullets;
+private:
+    BulletList _bullets;
 
 };
 #endif
