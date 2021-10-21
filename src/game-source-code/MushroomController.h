19f6cd8c0ecc858de7f3d8a86f2ff0c53a6a1686
Sbongile Mdaki <Sbongilep4@gmail.com>
2021-10-21 07:37:52 +0200

    Updated Mushroom Controller interface

--- src/game-source-code/MushroomController.h
+++ src/game-source-code/MushroomController.h
@@ -1,16 +1,27 @@
 #ifndef MUSHROOMCONTROLLER_H
 #define MUSHROOMCONTROLLER_H
+#include "Mushroom.h"
 
 
 class MushroomController
 {
     public:
         MushroomController();
-        ~MushroomController();
 
-    protected:
+        Mushrooms getVecOfMushrooms(){return mushrooms_;};
+
+        void createMushrooms(int number_of_mushrooms);
+
+        void createMushroom (int x, int y);
+
+        void update();
+
+        void setVecOfMushrooms(Mushrooms _mushrooms);
 
     private:
+        Mushrooms mushrooms_;
+
+        void deleteDeadMushroom(Mushrooms &mushrooms_);
 };
 
 #endif // MUSHROOMCONTROLLER_H
